e5e9ae46d88f1945cbf2fd1d7142cacfe2dad8de
Emily Pham <Emily.E.Pham@student.uts.edu.au>
2023-09-01 14:00:27 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,21 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     int frameCounter = 3;
+    int i;
     public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        int i = Random.Range(150, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
         frameCounter = frameCounter + 1;
-        Debug.Log(gameObject.name + "" + frameCounter);
+        Debug.Log(gameObject.name + ":" + frameCounter);
+
+        if (gameObject.CompareTag("Red") && frameCounter >= 100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        else if (gameObject.CompareTag("Blue") && (i >= 150 && i <= 250))
+        {
+            rend.enabled = false;
+        }
     }
 }
